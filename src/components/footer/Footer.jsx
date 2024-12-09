d0f91b39deba22a915cfe3a425e9097ed14462a2
DCODER <dlozanoc862@gmail.com>
2024-12-08 19:22:51 -0500

    ADD - Se agrega el componente footer con sus estilos

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,41 @@
+import "./footer.css";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+        <div className="footer__container container">
+            <h1 className="footer__title">Dcoder</h1>
+
+            <ul className="footer__list">
+                <li>
+                    <a href="#about" className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#skills" className="footer__link">Skills</a>
+                </li>
+
+                <li>
+                    <a href="#portfolio" className="footer__link">Projects</a>
+                </li>
+
+            </ul>
+
+            <div className="footer__social">
+                <a href="https://www.instagram.com/" className="footer__social-link" target="_blank">
+                    <i className="bx bxl-instagram"></i>
+                </a>
+
+                <a href="https://dribbble.com/" className="footer__social-link" target="_blank">
+                    <i className="bx bxl-linkedin"></i>
+                </a>
+
+                <a href="https://github.com/" className="footer__social-link" target="_blank">
+                    <i className="bx bxl-github"></i>
+                </a>
+            </div>
+
+        </div>
+    </footer>
   )
 }
